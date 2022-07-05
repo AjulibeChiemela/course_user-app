33db6bf3cfc0a1b1ea0f157e65f04b21176f3a24
Ajulibe Chiemela <chiemelaajulibe@yahoo.com>
2022-07-05 22:11:42 +0100

    implement ref logic

--- src/components/User/UserForm.js
+++ src/components/User/UserForm.js
@@ -1,37 +1,65 @@
-import React, { useState } from "react";
+//commented out is the same logic; but using state instead of refs
+// import React, { useState } from "react";
+import React, { useRef } from "react";
 import styles from "./UserForm.module.scss";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 
 const UserForm = (props) => {
-  const [userName, setUsername] = useState("");
-  const [age, setAge] = useState("");
+  // const [userName, setUsername] = useState("");
+  // const [age, setAge] = useState("");
 
-  const handleUsername = (e) => {
-    setUsername(e.target.value);
-  };
+  // const handleUsername = (e) => {
+  //   setUsername(e.target.value);
+  // };
 
-  const handleAgeInput = (e) => {
-    setAge(e.target.value);
-    e.target.value <= 0 && props.onValidateAge();
-  };
+  // const handleAgeInput = (e) => {
+  //   setAge(e.target.value);
+  //   e.target.value <= 0 && props.onValidateAge();
+  // };
+
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
 
   const handleUserData = (e) => {
     e.preventDefault();
-    if (userName.trim().length === 0 || age.trim().length === 0 || age <= 0) {
+    const enteredUsername = nameInputRef.current.value;
+    const enteredUserAge = ageInputRef.current.value;
+    enteredUserAge <= 0 && props.onValidateAge();
+
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredUserAge.trim().length === 0 ||
+      enteredUserAge <= 0
+    ) {
       props.onCheckValidate();
       return;
     } else {
       const newUserData = {
-        username: userName,
-        age: age,
+        username: enteredUsername,
+        age: enteredUserAge,
         id: Math.random().toString(),
       };
       props.onAddUser(newUserData);
 
-      setUsername("");
-      setAge("");
+      nameInputRef.current.value = "";
+      ageInputRef.current.value = "";
     }
+
+    // if (userName.trim().length === 0 || age.trim().length === 0 || age <= 0) {
+    //   props.onCheckValidate();
+    //   return;
+    // } else {
+    //   const newUserData = {
+    //     username: userName,
+    //     age: age,
+    //     id: Math.random().toString(),
+    //   };
+    //   props.onAddUser(newUserData);
+
+    //   setUsername("");
+    //   setAge("");
+    // }
   };
 
   return (
@@ -43,8 +71,9 @@ const UserForm = (props) => {
             type="text"
             name="username"
             id="username"
-            onChange={handleUsername}
-            value={userName}
+            // onChange={handleUsername}
+            // value={userName}
+            ref={nameInputRef}
           />
         </div>
         <div>
@@ -53,8 +82,9 @@ const UserForm = (props) => {
             type="number"
             name="age"
             id="age"
-            onChange={handleAgeInput}
-            value={age}
+            // onChange={handleAgeInput}
+            // value={age}
+            ref={ageInputRef}
           />
         </div>
         <Button type="submit">Add User</Button>
